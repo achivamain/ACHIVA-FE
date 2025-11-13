38e50d69f03ae02cc8737f063a03aa2f8a070369
Alchernix <alchernix149@naver.com>
2025-11-13 22:11:57 +0900

    fix(auth): 리프레시 토큰 에러 발생 시 자동 로그아웃 구현

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,8 +1,7 @@
-import getAuthStatus from "@/lib/getAuthStatus";
 import AuthHydrator from "@/features/auth/AuthHydrator";
 import Sidebar from "@/components/Sidebar";
-import { redirect } from "next/navigation";
-import { signOut } from "@/auth";
+import { auth } from "@/auth";
+import Logout from "@/components/Logout";
 
 export default async function Layout({
   children,
@@ -11,37 +10,20 @@ export default async function Layout({
   children: React.ReactNode;
   modal: React.ReactNode;
 }>) {
-  const auth = await getAuthStatus();
-  switch (auth.status) {
-    case "authenticated":
-      return (
-        <>
-          <AuthHydrator user={auth.user!} />
-          <Sidebar user={auth.user!} />
-          <div className="flex flex-col sm:ml-20 lg:ml-60 min-h-dvh">
-            {children}
-            {/* <Footer /> */}
-          </div>
-          {modal}
-        </>
-      );
-    // case "unauthenticated":
-    //   // 로그인되지 않았을 시, 사이드바 띄우지 않고, 본문에 마진 적용 x!!
-    //   return (
-    //     <div>
-    //       {children}
-    //       {modal}
-    //     </div>
-    //   );
-
-    default:
-      console.error("로그인 확인 에러", auth.error);
-      await signOut({ redirect: false });
-      return (
-        <div>
-          {children}
-          {modal}
-        </div>
-      );
+  const session = await auth();
+  if (session?.error) {
+    return <Logout />;
   }
+  console.log(session);
+  return (
+    <>
+      <AuthHydrator user={auth.user!} />
+      <Sidebar user={auth.user!} />
+      <div className="flex flex-col sm:ml-20 lg:ml-60 min-h-dvh">
+        {children}
+        {/* <Footer /> */}
+      </div>
+      {modal}
+    </>
+  );
 }
