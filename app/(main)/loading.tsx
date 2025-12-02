ebc8d8620e5e4375068b6cbeb1f6a5feab5e8db9
Alchernix <alchernix149@naver.com>
2025-12-02 14:43:00 +0900

    fix: 사이드바에서 화면 전환 중 로딩 화면 보여주도록 함

---
+++ app/(main)/loading.tsx
@@ -0,0 +1,9 @@
+import { LoadingIcon } from "@/components/Icons";
+
+export default function Page() {
+  return (
+    <div className="w-full h-dvh flex items-center justify-center">
+      <LoadingIcon size="size-8" color="text-theme" />
+    </div>
+  );
+}
---
+++ app/m/(main)/loading.tsx
@@ -0,0 +1,9 @@
+import { LoadingIcon } from "@/components/Icons";
+
+export default function Page() {
+  return (
+    <div className="flex-1 w-screen flex items-center justify-center">
+      <LoadingIcon size="size-8" color="text-theme" />
+    </div>
+  );
+}
