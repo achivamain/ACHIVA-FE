366ed4e381162f5e0e68ba1324a50237b42e3ac3
starligdt777 <math7822@gmail.com>
2026-01-31 18:45:20 +0900

    feature(categoryImage): 카테고리 카드에 이미지 연결, 색상 디자인 변경

--- features/category/CategoryCard.tsx
+++ features/category/CategoryCard.tsx
@@ -1,13 +1,21 @@
+import { Category, categoryImages } from "@/types/Categories";
+
 type CategoryCardProps = {
-  name: string;
+  name: Category;
   image?: string;
+  background: boolean;
 };
 
-export function CategoryCard({ name, image = "" }: CategoryCardProps) {
+export function CategoryCard({ name, image, background }: CategoryCardProps) {
+  const imageSrc = image ?? categoryImages[name];
+
   return (
     <div className="flex flex-col h-full justify-center item-center">
-      <div className="h-18 w-18 bg-[#D9D9D9] rounded-md">
-        {/*Image*/}
+      <div className={`h-18 w-18 ${background ? "bg-[#F3F3F3]" : "bg-white"} rounded-md flex items-center justify-center`}>
+        {imageSrc && (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img src={imageSrc} alt={name} />
+        )}
       </div>
       <span className="font-semibold text-[15px] text-center pt-0.5">{name}</span>
     </div>
--- types/Categories.ts
+++ types/Categories.ts
@@ -1,43 +1,102 @@
 export const categories = [
+  // 01. 근력 / 트레이닝
   "헬스",
   "맨몸운동",
   "크로스핏",
+  // 02. 유산소
   "러닝",
   "걷기",
   "사이클",
-  "등산",
+  // 03. 구기종목
   "축구",
-  "농구",
   "풋살",
+  "농구",
   "야구",
-  "볼링",
-  "골프",
   "배구",
   "럭비",
+  // 04. 라켓 / 스틱
   "탁구",
   "배드민턴",
   "테니스",
   "하키",
-  "복싱/킥복싱",
+  // 05. 정밀 / 과녁
+  "볼링",
+  "골프",
+  // 06. 격투 / 무술
+  "복싱",
   "MMA",
-  "유도",
   "태권도",
-  "주짓수",
   "레슬링",
+  "유도",
+  "주짓수",
   "검도/펜싱",
+  // 07. 수상
   "수영",
   "서핑",
   "조정",
+  // 08. 자세
   "요가",
   "필라테스",
-  "교정/재활",
   "스트레칭",
+  // 09. 등반
   "클라이밍",
+  "등산",
+  // 10. 스케이트 / 스키
   "롤러/인라인",
   "보드",
   "빙상",
   "스키/보드",
 ] as const;
+
+export const categoryImages: Record<Category, string> = {
+  // 01. 근력 / 트레이닝
+  "헬스": "/images/categories/01_weightTraining.svg",
+  "맨몸운동": "/images/categories/01_bodyweight.svg",
+  "크로스핏": "/images/categories/01_crossfit.svg",
+  // 02. 유산소
+  "러닝": "/images/categories/02_running.svg",
+  "걷기": "/images/categories/02_walking.svg",
+  "사이클": "/images/categories/02_cycle.svg",
+  // 03. 구기종목
+  "축구": "/images/categories/03_soccer.svg",
+  "풋살": "/images/categories/03_futsal.svg",
+  "농구": "/images/categories/03_basketball.svg",
+  "야구": "/images/categories/03_baseball.svg",
+  "배구": "/images/categories/03_volleyball.svg",
+  "럭비": "/images/categories/03_rugby.svg",
+  // 04. 라켓 / 스틱
+  "탁구": "/images/categories/04_tableTennis.svg",
+  "배드민턴": "/images/categories/04_badminton.svg",
+  "테니스": "/images/categories/04_tennis.svg",
+  "하키": "/images/categories/04_hockey.svg",
+  // 05. 정밀 / 과녁
+  "볼링": "/images/categories/05_bowling.svg",
+  "골프": "/images/categories/05_golf.svg",
+  // 06. 격투 / 무술
+  "복싱": "/images/categories/06_boxing.svg",
+  "MMA": "/images/categories/06_mma.svg",
+  "태권도": "/images/categories/06_taekwondo.svg",
+  "레슬링": "/images/categories/06_wrestling.svg",
+  "유도": "/images/categories/06_judo.svg",
+  "주짓수": "/images/categories/06_jujitsu.svg",
+  "검도/펜싱": "/images/categories/06_kendo.svg",
+  // 07. 수상
+  "수영": "/images/categories/07_swimming.svg",
+  "서핑": "/images/categories/07_surfing.svg",
+  "조정": "/images/categories/07_rowing.svg",
+  // 08. 자세
+  "요가": "/images/categories/08_yoga.svg",
+  "필라테스": "/images/categories/08_pilates.svg",
+  "스트레칭": "/images/categories/08_stretching.svg",
+  // 09. 등반
+  "클라이밍": "/images/categories/09_climbing.svg",
+  "등산": "/images/categories/09_hiking.svg",
+  // 10. 스케이트 / 스키
+  "롤러/인라인": "/images/categories/10_roller.svg",
+  "보드": "/images/categories/10_skateboard.svg",
+  "빙상": "/images/categories/10_iceSkate.svg",
+  "스키/보드": "/images/categories/10_ski.svg",
+};
 
 //소분류로 나눠진 버전
 export const groupedCategorys: { groupName: string; categories: Category[] }[] =
@@ -48,17 +107,15 @@ export const groupedCategorys: { groupName: string; categories: Category[] }[] =
     },
     {
       groupName: "유산소",
-      categories: ["러닝", "걷기", "사이클", "등산"],
+      categories: ["러닝", "걷기", "사이클"],
     },
     {
       groupName: "구기종목",
       categories: [
         "축구",
-        "농구",
         "풋살",
+        "농구",
         "야구",
-        "볼링",
-        "골프",
         "배구",
         "럭비",
       ],
@@ -68,14 +125,18 @@ export const groupedCategorys: { groupName: string; categories: Category[] }[] =
       categories: ["탁구", "배드민턴", "테니스", "하키"],
     },
     {
+      groupName: "정밀 / 과녁",
+      categories: ["볼링", "골프"],
+    },
+    {
       groupName: "격투 / 무술",
       categories: [
-        "복싱/킥복싱",
+        "복싱",
         "MMA",
-        "유도",
         "태권도",
-        "주짓수",
         "레슬링",
+        "유도",
+        "주짓수",
         "검도/펜싱",
       ],
     },
@@ -85,11 +146,11 @@ export const groupedCategorys: { groupName: string; categories: Category[] }[] =
     },
     {
       groupName: "자세",
-      categories: ["요가", "필라테스", "교정/재활", "스트레칭"],
+      categories: ["요가", "필라테스", "스트레칭"],
     },
     {
       groupName: "등반",
-      categories: ["클라이밍"],
+      categories: ["클라이밍", "등산"],
     },
     {
       groupName: "스케이트 / 스키",
