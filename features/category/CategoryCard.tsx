c4923515e9b96e16cdf4c98936c62d598b03edc8
starligdt777 <math7822@gmail.com>
2026-02-13 17:26:44 +0900

    feature(categoryImage): 카테고리 이미지 사이즈 조정

--- features/category/CategoryCard.tsx
+++ features/category/CategoryCard.tsx
@@ -1,4 +1,4 @@
-import { Category, categoryImages } from "@/types/Categories";
+import { Category, categoryImages, categoryImageHeights } from "@/types/Categories";
 
 type CategoryCardProps = {
   name: Category;
@@ -8,13 +8,19 @@ type CategoryCardProps = {
 
 export function CategoryCard({ name, image, background }: CategoryCardProps) {
   const imageSrc = image ?? categoryImages[name];
+  const imageHeight = categoryImageHeights[name];
 
   return (
     <div className="flex flex-col h-full justify-center item-center">
       <div className={`h-18 w-18 ${background ? "bg-[#F3F3F3]" : "bg-white"} rounded-md flex items-center justify-center`}>
         {imageSrc && (
           // eslint-disable-next-line @next/next/no-img-element
-          <img src={imageSrc} alt={name} />
+          <img
+            src={imageSrc}
+            alt={name}
+            style={{ height: imageHeight }}
+            className="w-auto object-contain"
+          />
         )}
       </div>
       <span className="font-semibold text-[15px] text-center pt-0.5">{name}</span>
